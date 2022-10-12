3753d58c1dbbde5b1bdfa26ec959455949d61f3e
GVal98 <GVal98@gmail.com>
2022-10-12 22:34:02 +0300

    feat: added "Load more" button

--- src/components/BlocksTable/BlocksTable.tsx
+++ src/components/BlocksTable/BlocksTable.tsx
@@ -1,19 +1,29 @@
-import { useContext, ReactNode } from 'react'
+import { useContext, ReactNode, useState } from 'react'
 import { useQuery } from '@tanstack/react-query'
 import { Web3Context } from '../../api/Web3Provider'
 import { BlockRow } from './BlockRow'
 import { BlocksTableView } from './BlocksTableView'
+import { Button } from '@mantine/core'
+
+const defaultBlocksCount = 10
 
 function BlocksTable() {
+  const [blocksCount, setBlocksCount] = useState(defaultBlocksCount)
+
   const web3 = useContext(Web3Context)
   const { data: latestBlockNumber } = useQuery(['latestBlockNumber'], () => web3.eth.getBlockNumber())
 
   if (!latestBlockNumber) return <>loading</>
 
-  let blockNumbers = new Array(10).fill(0).map((_, i) => latestBlockNumber - i)
+  let blockNumbers = new Array(blocksCount).fill(0).map((_, i) => latestBlockNumber - i)
   let blockRows = blockNumbers.map(blockNumber => <BlockRow key={blockNumber} blockNumber={blockNumber}/>)
 
-  return <BlocksTableView blockRows={blockRows}/>
+  return (
+    <>
+      <BlocksTableView blockRows={blockRows}/>
+      <Button onClick={() => (setBlocksCount(blocksCount + defaultBlocksCount))}>Load more</Button>
+    </>
+  )
 }
 
 export { BlocksTable }
