8479db3c956f98ba522e482ed6d37789f2f63630
GVal98 <GVal98@gmail.com>
2022-10-15 05:22:50 +0300

    feat: added style to address balance

--- src/components/common/Address/AddressView.tsx
+++ src/components/common/Address/AddressView.tsx
@@ -1,11 +1,13 @@
+import { Badge } from '@mantine/core'
+
 interface AddressViewProps {
   address: string,
   ethBalance: number | null
 }
 
 function AddressView(props: AddressViewProps) {
-  if (props.ethBalance !== null) return <>{props.address} : {props.ethBalance} ETH</>
-  return <>{props.address}</>
+  if (props.ethBalance !== null) return <>{props.address}<Badge ml="xs" variant="outline">{props.ethBalance} ETH</Badge></>
+  return <>{props.address}<Badge ml="xs" variant="outline">Loading</Badge></>
 }
 
 export { AddressView }
