69fe45b310340660e04b33ba1edba1bae43ec6c6
GVal98 <GVal98@gmail.com>
2022-10-27 04:24:22 +0300

    refactor: added Header component

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,35 +1,21 @@
 import { ReactNode } from 'react'
-import { AppShell, Header, Group, ActionIcon, useMantineColorScheme, Title, Container, Anchor,
-} from '@mantine/core'
-import Link from 'next/link'
+import { AppShell, Container } from '@mantine/core'
 import { Search } from './Search'
+import { Header } from './Header'
 
 interface LayoutProps {
   children: ReactNode
 }
-// todo: replace linkstyle with adding inside link
-function Layout(props: LayoutProps) {
-  const { colorScheme, toggleColorScheme } = useMantineColorScheme()
 
+function Layout(props: LayoutProps) {
   return (
     <AppShell
       padding="md"
-      header={(
-        <Header height={60}>
-          <Group sx={{ height: '100%' }} px={20} position="apart">
-            <Link href="/" passHref>
-              <Anchor variant="text">
-                <Title order={1} size="h3">ETH Explorer</Title>
-              </Anchor>
-            </Link>
-            <ActionIcon variant="default" onClick={() => toggleColorScheme()} size={30}>
-              {colorScheme === 'dark' ? '☀️' : '🌚' }
-            </ActionIcon>
-          </Group>
-        </Header>
-      )}
+      header={<Header />}
       styles={(theme) => ({
-        main: { backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[8] : theme.colors.gray[0] },
+        main: {
+          backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[8] : theme.colors.gray[0],
+        },
       })}
     >
       <Container size="xl">
